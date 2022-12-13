72517d32ff5a23c7936f381578083fc815f4cf7f
unknown <binyaalex@gmail.com>
2022-12-13 11:38:55 +0200

    finishes order success page

--- src/components/orderSuccess/OrderSuccess.js
+++ src/components/orderSuccess/OrderSuccess.js
@@ -1,10 +1,40 @@
+import styles from './OrderSuccess.module.scss';
+
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
 import V from '../../assets/icons/V.png'
 
 const OrderSuccess = () => {
     return (
-        <>
-            <img src={V} />
-        </>
+        <Container align='center'>
+            <img src={V} style={{padding: '85px 0 32px'}} />
+            <Typography className={styles.mainTitle}>
+                Order Placed!
+            </Typography>
+            <Typography className={styles.description}>
+                Your order will be processed as soon as we verify the availability of them items with the sellers.
+                <br></br>  
+                You can check the status of your order in the Orders section. 
+            </Typography>
+            <div>
+                <Button 
+                    sx={{ color: '#4640DE' }} 
+                    className={styles.backButton} 
+                    variant="text"
+                >
+                    <ArrowBackIosIcon sx={{ fontSize: '12px', marginRight: '12px' }} />
+                    Back to shopping
+                </Button>
+                <Button 
+                    sx={{color: 'white', backgroundColor: '#4640DE'}} 
+                    className={styles.orderButton} 
+                    variant="contained"
+                >Your Orders</Button>
+            </div>
+        </Container>
     )
 }
 
