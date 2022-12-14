1da36709542926e3f145f9a8e9d8810c0ba840f2
unknown <binyaalex@gmail.com>
2022-12-14 10:17:49 +0200

    making shopping stepper icon chang afer complete

--- src/components/shopping/ShoppingStepper.js
+++ src/components/shopping/ShoppingStepper.js
@@ -5,6 +5,13 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import SafetyCheckIcon from '@mui/icons-material/SafetyCheck';
+import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
+import LensIcon from '@mui/icons-material/Lens';
 
 import Steps from './Steps';
 import ShoppingMainTitle from './ShoppingMainTitle'
@@ -58,6 +65,89 @@ export default function ShoppingStepper() {
     setActiveStep(0);
   };
 
+  const QontoConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: '#784af4',
+      },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: '#784af4',
+      },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+      borderTopWidth: 3,
+      borderRadius: 1,
+    },
+  }));
+
+  const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+    backgroundColor: '#fff',
+    zIndex: 1,
+    color: '#C4CDD5',
+    // width: 50,
+    // height: 50,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...(ownerState.active && {
+    }),
+    ...(ownerState.completed && {
+    color: '#5C6AC4',
+    borderRadius: 'unset',
+    }),
+  }));
+
+  const StepIcon = ({ label }) => (
+    <div style={{ position: 'relative' }}>
+      <LensIcon />
+      <div style={{ color: 'white', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', textAlign: 'center', lineHeight: '24px' }}>{label}</div>
+    </div>
+  );
+
+
+  function ColorlibStepIcon(props) {
+    const { active, completed, className, } = props;
+    console.log(active);
+    console.log(completed);
+    let icon = <StepIcon label={props.icon} />
+    if (completed) {
+      icon =  <CheckBoxIcon />
+    }
+    const icons = {
+      1: icon,
+      2: icon,
+      3: icon,
+    };
+  
+    return (
+      <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
+        {icons[String(props.icon)]}
+      </ColorlibStepIconRoot>
+    );
+  }
+
+  ColorlibStepIcon.propTypes = {
+    /**
+     * Whether this step is active.
+     * @default false
+     */
+    active: PropTypes.bool,
+    className: PropTypes.string,
+    /**
+     * Mark the step as completed. Is passed to child components.
+     * @default false
+     */
+    completed: PropTypes.bool,
+    /**
+     * The label displayed in the step icon.
+     */
+    icon: PropTypes.node,
+  };
+
   return (
     <>
       {activeStep === 0 ? 
@@ -72,7 +162,7 @@ export default function ShoppingStepper() {
       : ""
       }
       <Box sx={{ width: '100%' }}>
-        <Stepper activeStep={activeStep}>
+        <Stepper activeStep={activeStep} connector={<QontoConnector />} >
           {steps.map((label, index) => {
             const stepProps = {};
             const labelProps = {};
@@ -86,7 +176,7 @@ export default function ShoppingStepper() {
             }
             return (
               <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
+                <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
               </Step>
             );
           })}
