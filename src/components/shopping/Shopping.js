39821d3819e073cd8103328b851192a6f1f2dc00
unknown <binyaalex@gmail.com>
2022-12-15 05:13:22 +0200

    shpping background

--- src/components/shopping/Shopping.js
+++ src/components/shopping/Shopping.js
@@ -1,11 +1,15 @@
+import styles from "./Shopping.module.scss";
+
+import Box from "@mui/material/Box";
+
 import ShoppingStepper from "./ShoppingStepper";
 
 const Shopping = () => {
 
   return (
-    <>
+    <Box className={styles.shoppingBackground} >
       <ShoppingStepper />
-    </>
+    </Box>
   );
 };
 
