74df0c461f107d636dc68b77847c91e415c077ea
unknown <binyaalex@gmail.com>
2022-12-15 10:27:57 +0200

    making account

--- src/components/settings/Account.js
+++ src/components/settings/Account.js
@@ -1,10 +1,18 @@
+import Grid from '@mui/material/Grid';
+
+import SettingsMenu from "./SettingsMenu";
 
 const Account = () => {
 
     return (
-      <>
-        Account
-      </>
+      <Grid container spacing={2}>
+        <Grid item xs={2.5}>
+            <SettingsMenu page='account' />
+        </Grid>
+        <Grid item xs={9.5}>
+            Account
+        </Grid>
+      </Grid>
     );
   };
   
